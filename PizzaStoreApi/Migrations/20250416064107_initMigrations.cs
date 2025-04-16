df8968571642c73dec7e7d4585ad387364a40b8d
kenneth løvgren <kenneth.l.thomsen@gmail.com>
2025-04-16 09:17:35 +0200

    Add initial migration files and database context for Pizza model

---
+++ PizzaStoreApi/Migrations/20250416064107_initMigrations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaStoreApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
